8aa6d47b6648d76ed4cf2cccece5ea81cfd623e9
Luk3rf7 <lkzeron@gmail.com>
2023-09-30 20:06:49 -0300

    feat:leet code 448

---
+++ LeetCode/5_subtractProduct.js
@@ -0,0 +1,20 @@
+/* 
+ give an integer number n,return difference between 
+ the product of its digits the sum of this digits
+
+
+ input  n = 234
+ outrput : 15
+
+ product of digits = 2 * 3 * 4 = 24
+ sum of digits = 2 + 3 + 4 = 9
+ result = 24- 9 = 15
+*/
+
+const subtractProductAndSum = function (n) {
+
+
+
+
+}
+console.log(subtractProductAndSum(123))
