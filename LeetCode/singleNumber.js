f1352101cd2e08eaf4e0079ce4809c56322199c3
Luk3rf7 <lkzeron@gmail.com>
2023-09-30 13:46:51 -0300

    feat:leetcode 136

---
+++ LeetCode/singleNumber.js
@@ -0,0 +1,24 @@
+/* 
+ * Single number :
+
+given a non-empty array of integers,every element appears
+twice except for one,Find that single one 
+
+Your algorithm should have a linear runtime complexity,could you
+implement iot whitout using extra memory 
+ 
+todo Example:
+
+input : [2,2,1]
+output : 1
+todo Example:
+
+input : [4,1,2,1,2]
+output : 4
+*/
+const arr = [5, 8, 9, 5, 5, 6, 6, 4, 5, 21, 5, 8]
+const singleNum = function (array) {
+
+};
+
+singleNum()
